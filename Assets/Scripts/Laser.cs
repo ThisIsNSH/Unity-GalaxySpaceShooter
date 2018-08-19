63ad4b1541924d5600c5cec0a615ee0bfb683b62
thisisnsh <hadanis.singh@gmail.com>
2018-08-19 19:27:33 +0530

    Files added

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour {
+
+    private float speed = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        if (transform.position.y >= 6.5f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    [SerializeField]
+    public float speed = 5.0f;
+    [SerializeField]
+    private GameObject Laser;
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+        Movement();
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            Instantiate(Laser, transform.position + new Vector3(0, 0.88f, 0), Quaternion.identity);
+        }
+    }
+
+    private void Movement()
+    {
+
+        float input = Input.GetAxis("Horizontal");
+        float input1 = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * speed * input * Time.deltaTime);
+        transform.Translate(Vector3.up * speed * input1 * Time.deltaTime);
+
+        if (transform.position.y >=0.1f)
+        {
+            transform.position = new Vector3(transform.position.x, 0.1f, 0);
+        }
+        else if (transform.position.y <= -3.5f)
+        {
+            transform.position = new Vector3(transform.position.x, -3.5f, 0);
+        }
+
+        if (transform.position.x >= 11.5f)
+        {
+            transform.position = new Vector3(-11.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x <= -11.5f)
+        {
+            transform.position = new Vector3(11.5f, transform.position.y, 0);
+        }
+
+    }
+}
